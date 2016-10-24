321894315a555a0a6157ddbf289d7687c01b12d0
Jan Boon <janboon438@gmail.com>
2016-10-24 10:26:32 +0200

    Reorganised code

--- js/Bowling.js
+++ js/Bowling.js
@@ -110,177 +110,177 @@ var Deck = (function () {
   }
   var maxZ = 52;
   function _card(i) {
-    var transform = prefix('transform');
-    var rank = i % 13 + 1;
-    var suit = i / 13 | 0;
-    var z = (52 - i) / 4;
-    var $el = createElement('div');
-    var $face = createElement('div');
-    var $back = createElement('div');
-    var isDraggable = false;
-    var isFlippable = false;
-    var self = { i: i, rank: rank, suit: suit, pos: i, $el: $el, mount: mount, unmount: unmount, setSide: setSide };
-    var modules = Deck.modules;
-    var module;
-    $face.classList.add('face');
-    $back.classList.add('back');
-    $el.style[transform] = translate(-z + 'px', -z + 'px');
-    self.x = -z;
-    self.y = -z;
-    self.z = z;
-    self.rot = 0;
-    self.setSide('back');
-    addListener($el, 'mousedown', onMousedown);
-    addListener($el, 'touchstart', onMousedown);
-    for (module in modules) {
-      addModule(modules[module]);
-    }
-    self.animateTo = function (params) {
-      var delay = params.delay;
-      var duration = params.duration;
-      var _params$x = params.x;
-      var x = _params$x === undefined ? self.x : _params$x;
-      var _params$y = params.y;
-      var y = _params$y === undefined ? self.y : _params$y;
-      var _params$rot = params.rot;
-      var rot = _params$rot === undefined ? self.rot : _params$rot;
-      var ease$$ = params.ease;
-      var onStart = params.onStart;
-      var onProgress = params.onProgress;
-      var onComplete = params.onComplete;
-      var startX, startY, startRot;
-      var diffX, diffY, diffRot;
-      animationFrames(delay, duration).start(function () {
-        startX = self.x || 0;
-        startY = self.y || 0;
-        startRot = self.rot || 0;
-        onStart && onStart();
-      }).progress(function (t) {
-        var et = ease[ease$$ || 'cubicInOut'](t);
-        diffX = x - startX;
-        diffY = y - startY;
-        diffRot = rot - startRot;
-        onProgress && onProgress(t, et);
-        self.x = startX + diffX * et;
-        self.y = startY + diffY * et;
-        self.rot = startRot + diffRot * et;
-        $el.style[transform] = translate(self.x + 'px', self.y + 'px') + (diffRot ? 'rotate(' + self.rot + 'deg)' : '');
-      }).end(function () {
-        onComplete && onComplete();
-      });
-    };
-    self.setRankSuit = function (rank, suit) {
-      var suitName = SuitName(suit);
-      $el.setAttribute('class', 'card ' + suitName + ' rank' + rank);
-    };
-    self.setRankSuit(rank, suit);
-    self.enableDragging = function () {
-      if (isDraggable) {
-        return;
-      }
-      isDraggable = true;
-      $el.style.cursor = 'move';
-    };
-    self.enableFlipping = function () {
-      if (isFlippable) {
-        return;
-      }
-      isFlippable = true;
-    };
-    self.disableFlipping = function () {
-      if (!isFlippable) {
-        return;
-      }
-      isFlippable = false;
-    };
-    self.disableDragging = function () {
-      if (!isDraggable) {
-        return;
-      }
-      isDraggable = false;
-      $el.style.cursor = '';
-    };
-    return self;
-    function addModule(module) {
-      module.card && module.card(self);
-    }
-    function onMousedown(e) {
-      var startPos = {};
-      var pos = {};
-      var starttime = Date.now();
-      e.preventDefault();
-      if (e.type === 'mousedown') {
-        startPos.x = pos.x = e.clientX;
-        startPos.y = pos.y = e.clientY;
-        addListener(window, 'mousemove', onMousemove);
-        addListener(window, 'mouseup', onMouseup);
-      } else {
-        startPos.x = pos.x = e.touches[0].clientX;
-        startPos.y = pos.y = e.touches[0].clientY;
-        addListener(window, 'touchmove', onMousemove);
-        addListener(window, 'touchend', onMouseup);
-      }
-      if (!isDraggable) {
-        return;
-      }
-      $el.style[transform] = translate(self.x + 'px', self.y + 'px') + (self.rot ? ' rotate(' + self.rot + 'deg)' : '');
-      $el.style.zIndex = maxZ++;
-      function onMousemove(e) {
-        if (!isDraggable) {
-          return;
-        }
-        if (e.type === 'mousemove') {
-          pos.x = e.clientX;
-          pos.y = e.clientY;
-        } else {
-          pos.x = e.touches[0].clientX;
-          pos.y = e.touches[0].clientY;
-        }
-        $el.style[transform] = translate(Math.round(self.x + pos.x - startPos.x) + 'px', Math.round(self.y + pos.y - startPos.y) + 'px') + (self.rot ? ' rotate(' + self.rot + 'deg)' : '');
-      }
-      function onMouseup(e) {
-        if (isFlippable && Date.now() - starttime < 200) {
-          self.setSide(self.side === 'front' ? 'back' : 'front');
-        }
-        if (e.type === 'mouseup') {
-          removeListener(window, 'mousemove', onMousemove);
-          removeListener(window, 'mouseup', onMouseup);
-        } else {
-          removeListener(window, 'touchmove', onMousemove);
-          removeListener(window, 'touchend', onMouseup);
-        }
-        if (!isDraggable) {
-          return;
-        }
-        self.x = self.x + pos.x - startPos.x;
-        self.y = self.y + pos.y - startPos.y;
-      }
-    }
-    function mount(target) {
-      target.appendChild($el);
-      self.$root = target;
-    }
-    function unmount() {
-      self.$root && self.$root.removeChild($el);
-      self.$root = null;
-    }
-    function setSide(newSide) {
-      if (newSide === 'front') {
-        if (self.side === 'back') {
-          $el.removeChild($back);
-        }
-        self.side = 'front';
-        $el.appendChild($face);
-        self.setRankSuit(self.rank, self.suit);
-      } else {
-        if (self.side === 'front') {
-          $el.removeChild($face);
-        }
-        self.side = 'back';
-        $el.appendChild($back);
-        $el.setAttribute('class', 'card');
-      }
-    }
+	var transform = prefix('transform');
+	var rank = i % 13 + 1;
+	var suit = i / 13 | 0;
+	var z = (52 - i) / 4;
+	var $el = createElement('div');
+	var $face = createElement('div');
+	var $back = createElement('div');
+	var isDraggable = false;
+	var isFlippable = false;
+	var self = { i: i, rank: rank, suit: suit, pos: i, $el: $el, mount: mount, unmount: unmount, setSide: setSide };
+	var modules = Deck.modules;
+	var module;
+	$face.classList.add('face');
+	$back.classList.add('back');
+	$el.style[transform] = translate(-z + 'px', -z + 'px');
+	self.x = -z;
+	self.y = -z;
+	self.z = z;
+	self.rot = 0;
+	self.setSide('back');
+	addListener($el, 'mousedown', onMousedown);
+	addListener($el, 'touchstart', onMousedown);
+	for (module in modules) {
+		addModule(modules[module]);
+	}
+	self.animateTo = function (params) {
+		var delay = params.delay;
+		var duration = params.duration;
+		var _params$x = params.x;
+		var x = _params$x === undefined ? self.x : _params$x;
+		var _params$y = params.y;
+		var y = _params$y === undefined ? self.y : _params$y;
+		var _params$rot = params.rot;
+		var rot = _params$rot === undefined ? self.rot : _params$rot;
+		var ease$$ = params.ease;
+		var onStart = params.onStart;
+		var onProgress = params.onProgress;
+		var onComplete = params.onComplete;
+		var startX, startY, startRot;
+		var diffX, diffY, diffRot;
+		animationFrames(delay, duration).start(function () {
+			startX = self.x || 0;
+			startY = self.y || 0;
+			startRot = self.rot || 0;
+			onStart && onStart();
+		}).progress(function (t) {
+			var et = ease[ease$$ || 'cubicInOut'](t);
+			diffX = x - startX;
+			diffY = y - startY;
+			diffRot = rot - startRot;
+			onProgress && onProgress(t, et);
+			self.x = startX + diffX * et;
+			self.y = startY + diffY * et;
+			self.rot = startRot + diffRot * et;
+			$el.style[transform] = translate(self.x + 'px', self.y + 'px') + (diffRot ? 'rotate(' + self.rot + 'deg)' : '');
+		}).end(function () {
+			onComplete && onComplete();
+		});
+	};
+	self.setRankSuit = function (rank, suit) {
+		var suitName = SuitName(suit);
+		$el.setAttribute('class', 'card ' + suitName + ' rank' + rank);
+	};
+	self.setRankSuit(rank, suit);
+	self.enableDragging = function () {
+		if (isDraggable) {
+			return;
+		}
+		isDraggable = true;
+		$el.style.cursor = 'move';
+	};
+	self.enableFlipping = function () {
+		if (isFlippable) {
+			return;
+		}
+		isFlippable = true;
+	};
+	self.disableFlipping = function () {
+		if (!isFlippable) {
+			return;
+		}
+		isFlippable = false;
+	};
+	self.disableDragging = function () {
+		if (!isDraggable) {
+			return;
+		}
+		isDraggable = false;
+		$el.style.cursor = '';
+	};
+	return self;
+	function addModule(module) {
+		module.card && module.card(self);
+	}
+	function onMousedown(e) {
+		var startPos = {};
+		var pos = {};
+		var starttime = Date.now();
+		e.preventDefault();
+		if (e.type === 'mousedown') {
+			startPos.x = pos.x = e.clientX;
+			startPos.y = pos.y = e.clientY;
+			addListener(window, 'mousemove', onMousemove);
+			addListener(window, 'mouseup', onMouseup);
+		} else {
+			startPos.x = pos.x = e.touches[0].clientX;
+			startPos.y = pos.y = e.touches[0].clientY;
+			addListener(window, 'touchmove', onMousemove);
+			addListener(window, 'touchend', onMouseup);
+		}
+		if (!isDraggable) {
+			return;
+		}
+		$el.style[transform] = translate(self.x + 'px', self.y + 'px') + (self.rot ? ' rotate(' + self.rot + 'deg)' : '');
+		$el.style.zIndex = maxZ++;
+		function onMousemove(e) {
+			if (!isDraggable) {
+				return;
+			}
+			if (e.type === 'mousemove') {
+				pos.x = e.clientX;
+				pos.y = e.clientY;
+			} else {
+				pos.x = e.touches[0].clientX;
+				pos.y = e.touches[0].clientY;
+			}
+			$el.style[transform] = translate(Math.round(self.x + pos.x - startPos.x) + 'px', Math.round(self.y + pos.y - startPos.y) + 'px') + (self.rot ? ' rotate(' + self.rot + 'deg)' : '');
+		}
+		function onMouseup(e) {
+			if (isFlippable && Date.now() - starttime < 200) {
+				self.setSide(self.side === 'front' ? 'back' : 'front');
+			}
+			if (e.type === 'mouseup') {
+				removeListener(window, 'mousemove', onMousemove);
+				removeListener(window, 'mouseup', onMouseup);
+			} else {
+				removeListener(window, 'touchmove', onMousemove);
+				removeListener(window, 'touchend', onMouseup);
+			}
+			if (!isDraggable) {
+				return;
+			}
+			self.x = self.x + pos.x - startPos.x;
+			self.y = self.y + pos.y - startPos.y;
+		}
+	}
+	function mount(target) {
+		target.appendChild($el);
+		self.$root = target;
+	}
+	function unmount() {
+		self.$root && self.$root.removeChild($el);
+		self.$root = null;
+	}
+	function setSide(newSide) {
+		if (newSide === 'front') {
+			if (self.side === 'back') {
+				$el.removeChild($back);
+			}
+			self.side = 'front';
+			$el.appendChild($face);
+			self.setRankSuit(self.rank, self.suit);
+		} else {
+			if (self.side === 'front') {
+				$el.removeChild($face);
+			}
+			self.side = 'back';
+			$el.appendChild($back);
+			$el.setAttribute('class', 'card');
+		}
+	}
   }
   function SuitName(suit) {
     return suit === 0 ? 'spades' : suit === 1 ? 'hearts' : suit === 2 ? 'clubs' : suit === 3 ? 'diamonds' : 'joker';
