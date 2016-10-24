b6933589403d101c327c1411434b48b66206ff7d
Jan Boon <janboon438@gmail.com>
2016-10-24 07:28:46 +0200

    first commit

---
+++ Bowling.js
@@ -0,0 +1,58 @@
+var prefix = Deck.prefix
+var transform = prefix('transform')
+var translate = Deck.translate
+var $container = document.getElementById('container')
+var $topbar = document.getElementById('topbar')
+var $sort = document.createElement('button')
+var $shuffle = document.createElement('button')
+var $bysuit = document.createElement('button')
+var $fan = document.createElement('button')
+var $Bowling = document.createElement('button')
+var $flip = document.createElement('button')
+$shuffle.textContent = 'Shuffle'
+$sort.textContent = 'Sort'
+$bysuit.textContent = 'By suit'
+$fan.textContent = 'Fan'
+$Bowling.textContent = 'Bowling'
+$flip.textContent = 'Flip'
+$topbar.appendChild($flip)
+$topbar.appendChild($shuffle)
+$topbar.appendChild($bysuit)
+$topbar.appendChild($fan)
+$topbar.appendChild($Bowling)
+$topbar.appendChild($sort)
+var deck = Deck()
+$shuffle.addEventListener('click', function () {
+  deck.shuffle()
+  deck.shuffle()
+})
+$sort.addEventListener('click', function () {
+  deck.sort()
+})
+$bysuit.addEventListener('click', function () {
+  deck.sort(true)
+  deck.bysuit()
+})
+$fan.addEventListener('click', function () {
+  deck.fan()
+})
+$flip.addEventListener('click', function () {
+  deck.flip()
+})
+$Bowling.addEventListener('click', function () {
+  deck.queue(function (next) {
+    deck.cards.forEach(function (card, i) {
+      setTimeout(function () {
+        card.setSide('back')
+      }, i * 7.5)
+    })
+    next()
+  })
+  deck.shuffle()
+  deck.shuffle()
+  deck.Bowling()
+})
+deck.mount($container)
+deck.intro()
+deck.sort()
+
