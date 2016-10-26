0f3922e26ef8380c382a4091bf1a8a3c13a54305
Jan Boon <janboon438@gmail.com>
2016-10-26 11:42:56 +0200

    Removed code not needed

--- Bowling.js
+++ Bowling.js
@@ -3,23 +3,10 @@ var transform = prefix('transform')
 var translate = Deck.translate
 var $container = document.getElementById('container')
 var $topbar = document.getElementById('topbar')
-var $sort = document.createElement('button')
-var $shuffle = document.createElement('button')
 var $Bowling = document.createElement('button')
-$shuffle.textContent = 'Shuffle'
-$sort.textContent = 'Sort'
 $Bowling.textContent = 'Bowling'
-$topbar.appendChild($shuffle)
 $topbar.appendChild($Bowling)
-$topbar.appendChild($sort)
 var deck = Deck()
-$shuffle.addEventListener('click', function () {
-  deck.shuffle()
-  deck.shuffle()
-})
-$sort.addEventListener('click', function () {
-  deck.sort()
-})
 $Bowling.addEventListener('click', function () {
   deck.queue(function (next) {
     deck.cards.forEach(function (card, i) {
