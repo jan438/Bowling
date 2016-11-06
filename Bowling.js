9e1a0a4ed7d988e0a2f8508d382f09ba9c981589
Jan Boon <janboon438@gmail.com>
2016-11-06 07:36:11 +0100

    Bowling button has been given an id

--- Bowling.js
+++ Bowling.js
@@ -1,12 +1,14 @@
-var prefix = Deck.prefix
-var transform = prefix('transform')
-var translate = Deck.translate
-var $container = document.getElementById('container')
-var $topbar = document.getElementById('topbar')
-var $Bowling = document.createElement('button')
-$Bowling.textContent = 'Bowling'
-$topbar.appendChild($Bowling)
-var deck = Deck()
+var prefix = Deck.prefix;
+var transform = prefix('transform');
+var translate = Deck.translate;
+var $container = document.getElementById('container');
+var $topbar = document.getElementById('topbar');
+var $Bowling = document.createElement('button');
+$Bowling.setAttribute("style", "background-color:Chartreuse; font-size:2em;");
+$Bowling.setAttribute("id", "solitaire");
+$Bowling.textContent = 'Bowling';
+$topbar.appendChild($Bowling);
+var deck = Deck();
 $Bowling.addEventListener('click', function () {
   deck.queue(function (next) {
     deck.cards.forEach(function (card, i) {
@@ -19,8 +21,7 @@ $Bowling.addEventListener('click', function () {
   deck.shuffle()
   deck.shuffle()
   deck.Bowling()
-})
-deck.mount($container)
-deck.intro()
-deck.sort()
-
+});
+deck.mount($container);
+deck.intro();
+deck.sort();
